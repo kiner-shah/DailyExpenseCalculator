328f52dba97482a5e563078de412efeaea6cefce
Kiner Shah <kiner.shah1@gmail.com>
2018-01-27 10:55:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+/**********************************************************************************
+ * Author: 	Kiner Shah
+ * Date: 	26-Jan-2018
+ **********************************************************************************/
+	session_start();
+	error_reporting(E_ERROR | E_PARSE);
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="design.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="logic.js"></script>
+		<script>
+			function generatePDFDocWithTable() {
+				var tablevar = document.getElementById("table-one");
+				var balancevar = document.getElementById("total-value").innerHTML;
+				var jsonObj = [];
+				for(var i = 0; i < tablevar.rows.length; i++) {
+					jsonObj[i] = [];
+					for(var j = 0; j < tablevar.rows[i].cells.length; j++) {
+						jsonObj[i][j] = tablevar.rows[i].cells[j].innerHTML;
+					}
+				}
+				var targetURL = "pdf_generator.php?";
+				var targetData = "generatePDF=" + encodeURIComponent(jsonObj)+"&generatePDFData=" + encodeURIComponent(balancevar);
+				alert(targetData);
+				var xmlhttp = new XMLHttpRequest();
+				xmlhttp.onreadystatechange = function() {
+					if(this.readyState == 4 && this.status == 200) {
+						var responseJSONObj = eval(this.reponseText);
+						if(responseJSONObj == true) {
+							console.log("Successfully generated PDF");
+						}
+						else {
+							console.log("Problem in generating PDF");	
+						}
+					}
+				}
+				xmlhttp.open("POST", targetURL, true);
+				xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+				xmlhttp.send(targetData);
+			}
+		</script>
+	</head>
+	<body>
+		<div class="top-header">
+			<img src="logo.png" class="inline-display" style="width: 80px; height: 80px;">
+			<h1 class="inline-display">Daily Expense Calculator</h1>
+		</div>
+		<div class="entry-div">
+			<input type="text" id="details-box" placeholder="E.g. Food">
+			<input type="text" id="cost-box" placeholder="E.g. 200">
+			<input type="button" id="spent-button" style="background-color: indianred" value="Spent">
+			<input type="button" id="earned-button" style="background-color: forestgreen" value="Earned">
+			<p id="error-message"></p>
+		</div>
+		<div class="balance-div">
+			<p class="inline-display">Total: </p>
+			<p class="inline-display" id="total-value"></p>
+		</div>
+		<div class="export-div">
+			<!--<form align="center" method="get" action="pdf_generator.php">
+				<input type="submit" id="generate-pdf" name='submit-button' style="background-color: gray" value="Export to PDF" onclick="generatePDFDocWithTable()">
+				<p id="loading-message"></p>
+			</form>-->
+			<input type="button" id="generate-pdf" name='submit-button' value="Export to PDF" style="background-color: navy" onclick="generatePDFDocWithTable()">
+			<p><!-- Blank paragraph --></p>
+		</div>
+		<div class="table-div" name="target-table">
+			<table id="table-one" align="center">
+				<thead id="table-one-head">
+					<th>Description</th>
+					<th>Amount</th>
+				</thead>
+				<tbody id="table-one-body">
+				</tbody>
+			</table>
+		</div>
+	</body>
+</html>
---
+++ logic.js
@@ -0,0 +1,95 @@
+var total_balance = 0;
+$(document).ready(function() {
+	$('#total-value').append(total_balance);
+	$('#total-value').css('float', 'right');
+	$('#total-value').css('padding-right', '10px');
+	$('#table-one').hide();
+	$('#generate-pdf').hide();
+	$('#spent-button').click(function() {
+		var details_box = $('#details-box').val();
+		var cost_box = $('#cost-box').val();
+		if(!details_box || details_box.length == 0) {
+			$('#error-message').append("Error: blank description");
+			$('#error-message').css('color', 'red');
+			$('#error-message').css('font-family', 'sans-serif');
+			$('#details-box').css('border', '2px solid red');
+		}
+		else {
+			$('#error-message').empty();
+			$('#details-box').css('border', '1px solid gray');
+
+			if(!cost_box || cost_box.length == 0) {
+				$('#error-message').append("Error: no amount entered");
+				$('#error-message').css('color', 'red');
+				$('#error-message').css('font-family', 'sans-serif');
+				$('#cost-box').css('border', '2px solid red');
+			}
+			else {
+				$('#error-message').empty();
+				$('#cost-box').css('border', '1px solid gray');
+
+				var cost_regex = new RegExp(/^[0-9]+([.][0-9]+)?$/);
+				if(cost_regex.test(cost_box) == true) {
+					if($('#table-one').css('display') == 'none') {
+						$('#table-one').show();
+						$('#generate-pdf').show();
+						$('#generate-pdf').css('background-color', 'navy');
+					}
+					$('#table-one').append('<tr class="spent-row"><td>' + details_box + '</td><td>-' + cost_box + '</td></tr>');
+					total_balance = total_balance - parseFloat(cost_box);
+					$('#total-value').empty();
+					$('#total-value').append(total_balance.toFixed(2));
+				}
+				else {
+					$('#error-message').append("Error: invalid amount entered. Make sure there are no + or - signs and the value is proper");
+					$('#error-message').css('color', 'red');
+					$('#error-message').css('font-family', 'sans-serif');
+					$('#cost-box').css('border', '2px solid red');
+				}
+			}
+		}
+	});
+	$('#earned-button').click(function() {
+		var details_box = $('#details-box').val();
+		var cost_box = $('#cost-box').val();
+		if(!details_box || details_box.length == 0) {
+			$('#error-message').append("Error: blank description");
+			$('#error-message').css('color', 'red');
+			$('#error-message').css('font-family', 'sans-serif');
+			$('#details-box').css('border', '2px solid red');
+		}
+		else {
+			$('#error-message').empty();
+			$('#details-box').css('border', '1px solid gray');
+
+			if(!cost_box || cost_box.length == 0) {
+				$('#error-message').append("Error: no amount entered");
+				$('#error-message').css('color', 'red');
+				$('#error-message').css('font-family', 'sans-serif');
+				$('#cost-box').css('border', '2px solid red');
+			}
+			else {
+				$('#error-message').empty();
+				$('#cost-box').css('border', '1px solid gray');
+				var cost_regex = new RegExp(/^[0-9]+([.][0-9]+)?$/);
+				if(cost_regex.test(cost_box) == true) {
+					if($('#table-one').css('display') == 'none') {
+						$('#table-one').show();
+						$('#generate-pdf').show();
+						$('#generate-pdf').css('background-color', 'navy');
+					}
+					$('#table-one').append('<tr class="earned-row"><td>' + details_box + '</td><td>' + cost_box + '</td></tr>');
+					total_balance = total_balance + parseFloat(cost_box);
+					$('#total-value').empty();
+					$('#total-value').append(total_balance.toFixed(2));
+				}
+				else {
+					$('#error-message').append("Error: invalid amount entered. Make sure there are no + or - signs and the value is proper");
+					$('#error-message').css('color', 'red');
+					$('#error-message').css('font-family', 'sans-serif');
+					$('#cost-box').css('border', '2px solid red');
+				}
+			}
+		}
+	});
+});
